068b69d8dd27b52be5f502f9e0fc1abb9a68fc32
admin <admin@DESKTOP-1GC9IUF>
2021-09-14 11:17:36 +0900

    second

--- firstProject/src/firstProject/HelloWorld.java
+++ firstProject/src/firstProject/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scn =new Scanner(System.in);
 		System.out.println("Hello, World!!!");
-		
+		scn.nextInt();
 		
 	}
 }
